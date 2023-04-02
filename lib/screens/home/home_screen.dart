82bf098972f5ba4ef65e35327269bd4a63c0b959
LucasGit72 <lucas.Renard72450@gmail.com>
2023-04-02 22:06:54 +0200

    carousel/lottie

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,14 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'dart:async';
 
+import 'package:lottie/lottie.dart';
+
 Player player = Player();
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+final List<String> txtList = [
+  'Le Saviez vous! Vous pouvez voir le descriptif de chaque équipements en maintenant le doigt sur l\'équipement de votre choix.',
+  'Le Profil permet de voir chaque statistiques du joueur',
+  'Des nouvelles quêtes chaque jour pour vous donner encore plus envie de jouer'
 ];
 
 class HomeScreen extends StatefulWidget {
@@ -89,72 +88,100 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: [
-          Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 30,
-                  horizontal: 20,
-                ),
-                child: Column(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            child: Column(children: [
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 30,
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Text(
-                          "${AppLocalizations.of(context)!.hello} ${player.name}",
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    player.lastAfkIncome != 0 ? Row(
-                      children: [
-                        Flexible(
-                          child: Text(
-                            "${AppLocalizations.of(context)!.afkIncome} ${player.lastAfkIncome} ${AppLocalizations.of(context)!.goldsLower}",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey[600],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ) : Container(),
-                    CarouselSlider(
-                      options: CarouselOptions(
-                        height: 400.0,
+                    Text(
+                      "${AppLocalizations.of(context)!.hello} ${player.name}",
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
                       ),
-                      items: imgList
-                          .map(
-                            (item) => Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 10.0,
-                              ),
-                              child: Center(
-                                child: Image.network(
-                                  item,
-                                  fit: BoxFit.cover,
-                                  width: 1000,
-                                ),
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    ),
+                    )
                   ],
                 ),
-              ),
-            ],
+                const SizedBox(height: 5),
+                player.lastAfkIncome != 0
+                    ? Row(
+                        children: [
+                          Flexible(
+                            child: Text(
+                              "${AppLocalizations.of(context)!.afkIncome} ${player.lastAfkIncome} ${AppLocalizations.of(context)!.goldsLower}",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : Container(),
+                CarouselSlider.builder(
+                  itemCount: txtList.length,
+                  itemBuilder:
+                      (BuildContext context, int index, int realIndex) {
+                    final text = txtList[index];
+                    return Container(
+                      color: Colors.grey,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.lightbulb_outline_rounded,
+                              size: 26,
+                              color: Colors.yellow,
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              text,
+                              style: const TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  options: CarouselOptions(
+                    height: 100,
+                    aspectRatio: 1.0,
+                    viewportFraction: 0.9,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 3),
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 600),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                Align(
+                  child: Lottie.asset(
+                    'assets/lottie/animation.json',
+                    height: 500,
+                    width: 350,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-    );
+        ])));
   }
 }
